1f8a8c88c6a6bfae9f89f702b76990b2f110f1b5
Isai Alcântara <11619375+isfalcantara@users.noreply.github.com>
2020-09-29 22:19:04 -0300

    Adds Card model

---
+++ app/Models/Card.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Card extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $fillable = [
+        'title',
+        'description',
+        'card_list_id',
+    ];
+
+    /**
+     * The card belongs to a card list
+     */
+    public function cardList()
+    {
+        return $this->belongsTo('App\Models\CardList');
+    }
+}
