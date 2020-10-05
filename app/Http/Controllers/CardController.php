cb7199c733e5fdcdcf66b4d1436918336cf8ff91
Judison Godinho de Sousa <judison@outlook.com>
2020-10-05 00:18:42 -0300

    Added Card crud

---
+++ app/Http/Controllers/CardController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CardList;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create($id)
+    {
+        return view('card.create', compact('id'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, $id)
+    {
+        DB::beginTransaction();
+        try {
+            $cardList = CardList::findOrFail($id);
+            $lastCard = $cardList->cards()->orderBy('rank', 'desc')->first();
+            $card = $cardList->cards()->create([
+                'title' => $request->title,
+                'description' => $request->description,
+                'rank' => $lastCard?$lastCard->rank + 1:1,
+            ]);
+            if ($request->hasFile('attachment')) {
+                $storage = $request->attachment->store('attachments');
+                dd($storage);
+            }
+            DB::commit();
+            return redirect()->route('dashboard::boards.show', $cardList->board);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            dd($e->getMessage());
+//            return back();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
